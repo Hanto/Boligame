9a8125a9fdeb21c9c78672fa22c37a7a80b8cf3d
Ivan Delgado Huerta <jhanto@gmail.com>
2024-08-09 17:17:34 +0200

    3

--- core/src/main/java/com/boligame/Main.java
+++ core/src/main/java/com/boligame/Main.java
@@ -3,20 +3,22 @@ package com.boligame;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
 
     private SpriteBatch batch;
-    private Texture image;
     private AssetManager assetManager;
     private Stage worldStage ;
 
@@ -27,6 +29,7 @@ public class Main extends ApplicationAdapter {
         worldStage = new Stage();
         assetManager = new AssetManager();
         assetManager.load("atlas/Atlas.atlas", TextureAtlas.class);
+        assetManager.load("fonts/20.fnt", BitmapFont.class);
         assetManager.finishLoading();
 
         TextureAtlas atlas = assetManager.get("atlas/Atlas.atlas", TextureAtlas.class);
@@ -34,9 +37,15 @@ public class Main extends ApplicationAdapter {
         TextureRegion[][] splited = texture.split(32, 32);
         TextureRegion playerImage = splited[0][0];
 
-        Image image = new Image(playerImage);
+        assetManager.finishLoading();
 
+        Image image = new Image(playerImage);
+        BitmapFont font = assetManager.get("fonts/20.fnt", BitmapFont.class);
+        Label label = new Label("HOLA BOLICHERO", new Label.LabelStyle(font, Color.ORANGE));
+
+        label.setPosition(100f, 100f);
         worldStage.addActor(image);
+        worldStage.addActor(label);
 
     }
 
@@ -54,7 +63,6 @@ public class Main extends ApplicationAdapter {
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
         worldStage.dispose();
     }
 }
